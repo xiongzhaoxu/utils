72a33f24efb182d7cad560efc017775819d1747e
sinry <1093803222@qq.com>
2020-04-30 16:42:56 +0800

    modify

---
+++ mvn/README.md
@@ -0,0 +1,15 @@
+# Usage
+
+pom.xml
+```xml
+
+    <repositories>
+        <repository>
+            <id>sinry-maven-repo</id>
+            <url>https://raw.githubusercontent.com/isinry/maven/master/repository/</url>
+        </repository>
+    </repositories>
+```
+
+> https://raw.githubusercontent.com/ 该地址如有打不开则需要配置host
+> 参考：[https://www.ioiox.com/archives/62.html](https://www.ioiox.com/archives/62.html)
--- push-maven.sh
+++ push-maven.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
-
-version=1.0.8-RELEASE
+version=1.0.10-RELEASE
 mvn clean
-mvn package
-cd ./target/
-mvn install:install-file -Dfile=utils-$version.jar -DgroupId=com.sinry -DartifactId=utils -Dversion=$version -Dpackaging=jar
-cd ~/.m2/repository/com/sinry
+mvn deploy -DaltDeploymentRepository=sinry-mvn-repo::default::file:mvn/repository
+#mvn package
+#cd ./target/
+#mvn install:install-file -Dfile=utils-$version.jar -DgroupId=com.sinry -DartifactId=utils -Dversion=$version -Dpackaging=jar
+
+cd mvn
+git init
+git remote add origin https://github.com/isinry/maven.git
 git remote set-url origin https://github.com/isinry/maven.git
+git pull origin master --allow-unrelated-histories
 git add .
 git commit -m "add version"
-git push
-cd ~/project/java/work/code/utils
+git push -u origin master
